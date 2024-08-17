0e214cb1995e0727ff6cf21e29a71ffd18b06b20
snwikaij <wimkaijseru@outlook.com>
2024-08-17 13:59:53 +0200

    add abctoz remove other

--- R/ptoz.R
+++ R/ptoz.R
@@ -7,7 +7,8 @@
 #' @param names Additional names
 #' @param alpha alpha level
 #'
-ptoz <- function(coef=NULL, sei=NULL, zi=NULL, pi=NULL, names=NULL, alpha=0.05){
+ptoz <- function(coef=NULL, sei=NULL, zi=NULL, pi=NULL, names=NULL, alpha=0.05,
+                 absolute=F, xlim=NULL, ylim=NULL){
 
   zint <- abs(qnorm(alpha/2))
 
@@ -39,46 +40,48 @@ ptoz <- function(coef=NULL, sei=NULL, zi=NULL, pi=NULL, names=NULL, alpha=0.05){
         df <- data.frame(names=names, coef=coef, se=se, z=z, p=p, ll=ll, ul=ul)
 
         df2 <- data.frame(coef=df$coef,
-                          lae=log(abs(df$coef)),
-                          logaz=log(abs(df$coef/df$se)),
+                          z=df$coef/df$se,
                           pre=1/df$se,
-                          logpre=log(1/df$se),
                           se=df$se,
                           s=names)
 
-        df2$sig <- ifelse(df2$logaz>log(zint),"sig","nonsig")
+        df2$sig <- ifelse(abs(df2$z)>abs(zint),"sig","nonsig")
 
-        df2     <- df2[!(is.infinite(df2$lae) | is.infinite(df2$pre)),]
+        df2     <- df2[!(is.infinite(df2$coef) | is.infinite(df2$pre)),]
         spldf   <- split(df2, df2$s)
 
-        lint <- lapply(spldf, function(x) c(lm(lae ~ I(se^2), weight = pre, data = x)$coef[1],
-                                            lm(lae~logpre, data = x)$coef[1],
-                                            lm(lae~logpre, data = x)$coef[2]))
+        rcoef             <- quantile(abs(df2$coef), c(.005, .995))
+        yl_pos            <- seq(0, ifelse(!is.null(ylim), ylim[2], rcoef[2]), length.out=50)
+        yl_neg            <- seq(0, ifelse(!is.null(ylim), ylim[1], -1*rcoef[2]), length.out=50)
+        sigline_pos       <- data.frame(coef=yl_pos, se=abs(yl_pos/1.96))
+        sigline_neg       <- data.frame(coef=yl_neg, se=abs(yl_neg/1.96))
+        sigline_pos$linvse<- 1/sigline_pos$se
+        sigline_neg$linvse<- 1/sigline_neg$se
 
-        rlogpre           <- quantile(df2$logpre, c(.005, .995))
-        xlp               <- seq(rlogpre[1], rlogpre[2], length.out=10)
-        b_lines           <- cbind.data.frame(do.call(rbind, lapply(lint, function(x) cbind(xlp, x[2]+x[3]*xlp))), rep(names(lint), each=50))
-        colnames(b_lines) <- c("logpre", "lae", "s")
+        if(!is.null(ylim)){ylim_plot <- ylim(ylim)}else{ylim_plot <- NULL}
+        if(!is.null(xlim)){xlim_plot <- xlim(xlim)}else{xlim_plot <- NULL}
 
-        adjline           <- setNames(cbind.data.frame(do.call(rbind, lint))[-c(2:3)], "intercept")
-        adjline$slope     <- 0
-        adjline$s         <- rownames(adjline)
-
-        rlae              <- quantile(df2$lae, c(.005, .995))
-        ylp               <- seq(exp(rlae[1]), exp(rlae[2]), length.out=10)
-        sigline           <- data.frame(lae=log(ylp), se=ylp/1.96)
-        sigline$linvse    <- log(1/sigline$se)
-
-        plot1 <- ggplot(df2, aes(x=logpre, y=lae, col=sig))+
-          geom_point(alpha=0.4)+xlab("Ln(1/se)")+ylab("Ln(|Est.|)")+
-          geom_line(data=sigline, aes(linvse, lae), lty=2, col="tomato4")+
-          #geom_line(data=b_lines, aes(logpre, lae), lty=1, col="tomato4")+
-          geom_abline(data=adjline, aes(intercept = intercept, slope = slope), col="blue4") +
+        if(absolute==F){
+        plot1 <- ggplot(df2, aes(x=pre, y=coef, col=sig))+
+          geom_point(alpha=0.4)+xlab("1/se")+ylab("Est.")+ylim_plot+xlim_plot+
+          geom_line(data=sigline_pos, aes(linvse, coef), lty=2, col="tomato4")+
+          geom_line(data=sigline_neg, aes(linvse, coef), lty=2, col="tomato4")+
+          geom_hline(yintercept = 0, lty=3, col="tomato4")+
           scale_color_manual(breaks = c("sig", "nonsig"),
                              values = c("tomato3", "dodgerblue3"))+
           theme_classic()+
           facet_wrap(.~s, scales = "free")+
-          theme(legend.position = "none")
+          theme(legend.position = "none")}else{
+
+        plot1 <- ggplot(df2, aes(x=pre, y=abs(coef), col=sig))+
+          geom_point(alpha=0.4)+xlab("1/se")+ylab("|Est.|")+ylim_plot+xlim_plot+
+          geom_line(data=sigline_pos, aes(linvse, coef), lty=2, col="tomato4")+
+          geom_hline(yintercept = 0, lty=3, col="tomato4")+
+          scale_color_manual(breaks = c("sig", "nonsig"),
+                             values = c("tomato3", "dodgerblue3"))+
+          theme_classic()+
+          facet_wrap(.~s, scales = "free")+
+          theme(legend.position = "none")}
 
         }
     }else{df    <- data.frame(names=1:length(coef), coef=coef, se=se, z=z, p=p, ll=ll, ul=ul)

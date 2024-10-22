5e9ec1e876cc171e78ce3bbd070385269ad41b97
snwikaij <wimkaijseru@outlook.com>
2024-10-22 13:22:35 +0200

    grid???

--- R/pdplot.R
+++ R/pdplot.R
@@ -28,7 +28,7 @@
 #' @importFrom ggplot2 element_blank
 #' @importFrom ggplot2 element_text
 #' @importFrom ggplot2 element_line
-#' @importFrom ggplot2 unit
+#' @importFrom grid unit
 #'
 #' @export
 pdplot <- function(object, interval=0.9, display="b1",
